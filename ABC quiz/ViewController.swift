3384b2dc095309e590677888075d8d21593f2fd2
ufoxcode <ufoxcode@gmail.com>
2019-11-20 22:46:51 +0800

    Initial Commit

---
+++ ABC quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABC quiz
+//
+//  Created by UFO Xcode on 2019/11/20.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
